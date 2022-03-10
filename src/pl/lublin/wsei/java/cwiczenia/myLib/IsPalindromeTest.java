80542e9d4caff2bbde073acd93296ccf3c69a6ef
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-10 11:20:27 +0100

    Zad1. d) metoda isPalindrome i jej klasa testująca

---
+++ src/pl/lublin/wsei/java/cwiczenia/myLib/IsPalindromeTest.java
@@ -0,0 +1,23 @@
+package pl.lublin.wsei.java.cwiczenia.myLib;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class IsPalindromeTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word;
+
+        do {
+            System.out.println("Czy to palindrom? Jakie słowo sprawdzić?");
+            word = scanner.nextLine();
+            if (StringFun.isPalindrome(word)) {
+                System.out.printf("Słowo \"%s\" JEST palindromem\n", word);
+            } else {
+                System.out.printf("Słowo \"%s\" NIE JEST palindromem\n", word);
+            }
+            System.out.println("Wciśnij [q] aby zamknąć lub [enter] aby kontynuować");
+            word = scanner.nextLine();
+        } while (!word.equals("q"));
+    }
+}
--- src/pl/lublin/wsei/java/cwiczenia/myLib/StringFun.java
+++ src/pl/lublin/wsei/java/cwiczenia/myLib/StringFun.java
@@ -3,44 +3,57 @@ package pl.lublin.wsei.java.cwiczenia.myLib;
 import org.apache.commons.lang3.StringUtils;
 
 public class StringFun {
-  public static String anarchize(String word){
-      String result = "";
-      if(Character.isLowerCase(word.charAt(0))){
-          for (int i = 0; i<word.length(); i++){
-              if (i%2 == 0){
-                  result+=Character.toUpperCase(word.charAt(i));
-              }
-              else {
-                  result += Character.toLowerCase(word.charAt(i));
-              }
-          }
-      }
-      if(Character.isUpperCase(word.charAt(0))){
-          for (int i = 0; i<word.length(); i++){
-              if (i%2 == 0){
-                  result += Character.toLowerCase(word.charAt(i));
-              }
-              else {
-                  result+=Character.toUpperCase(word.charAt(i));
-              }
-          }
-      }
-      return result;
-  }
+    public static String anarchize(String word) {
+        String result = "";
+        if (Character.isLowerCase(word.charAt(0))) {
+            for (int i = 0; i < word.length(); i++) {
+                if (i % 2 == 0) {
+                    result += Character.toUpperCase(word.charAt(i));
+                } else {
+                    result += Character.toLowerCase(word.charAt(i));
+                }
+            }
+        }
+        if (Character.isUpperCase(word.charAt(0))) {
+            for (int i = 0; i < word.length(); i++) {
+                if (i % 2 == 0) {
+                    result += Character.toLowerCase(word.charAt(i));
+                } else {
+                    result += Character.toUpperCase(word.charAt(i));
+                }
+            }
+        }
+        return result;
+    }
 
-  public static String camelize(String word){
+    public static String camelize(String word) {
         String text = word.toLowerCase();
         String[] arr = StringUtils.split(text);
-        for(int i = 1; i<arr.length; i++){
-          arr[i] = StringUtils.capitalize(arr[i]);
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = StringUtils.capitalize(arr[i]);
         }
         return StringUtils.join(arr);
-  }
+    }
 
-  public static String decamelize(String word){
-      String[] strings = StringUtils.splitByCharacterTypeCamelCase(word);
-      return StringUtils.join(strings, " ");
-  }
+    public static String decamelize(String word) {
+        String[] strings = StringUtils.splitByCharacterTypeCamelCase(word);
+        return StringUtils.join(strings, " ");
+    }
 
+    public static boolean isPalindrome(String word) {
+        String a,b;
+        if(StringUtils.containsWhitespace(word)) {
+            word = StringUtils.deleteWhitespace(word);
+        }
+        for (int i = 0; i < word.length() / 2; i++) {
+            a = String.valueOf(word.charAt(i));
+            b = String.valueOf(word.charAt(word.length() - 1 - i));
+            if (StringUtils.compareIgnoreCase(a, b) != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 
 }
