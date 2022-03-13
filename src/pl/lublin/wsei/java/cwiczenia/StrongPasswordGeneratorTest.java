d050f9f757e30976ffdede34782fb95f33de1b24
Emilia Duda <emilia.duda1995@gmail.com>
2022-03-13 14:36:35 +0100

    Zad1. f) klasa testująca z interfejsem użytkownika

--- src/pl/lublin/wsei/java/cwiczenia/StrongPasswordGeneratorTest.java
+++ src/pl/lublin/wsei/java/cwiczenia/StrongPasswordGeneratorTest.java
@@ -2,17 +2,37 @@ package pl.lublin.wsei.java.cwiczenia;
 
 import pl.lublin.wsei.java.cwiczenia.myLib.StrongPasswordGenerator;
 
+import java.util.Scanner;
+
 
 public class StrongPasswordGeneratorTest {
     public static void main(String[] args) {
-        String password = new StrongPasswordGenerator(
-                10
-                ,true,
-                false,
-                true
-                ,false,
-                "&!#").generate();
-        System.out.println(password);
+        Scanner scan = new Scanner(System.in);
+        int length;
+        String choice;
+        boolean lower,upper,digits,chars;
+        String allowedChars="";
+        System.out.println("Skonfiguruj swoje hasło");
+        System.out.println("podaj długość: ");
+        length = scan.nextInt();
+        System.out.println("czy ma się składać z małych liter? t/n");
+        scan.nextLine();
+        choice = scan.nextLine();
+        lower = choice.equals("t");
+        System.out.println("czy ma się składać z dużych liter? t/n");
+        choice = scan.nextLine();
+        upper = choice.equals("t");
+        System.out.println("czy ma mieć liczby? t/n");
+        choice = scan.nextLine();
+        digits = choice.equals("t");
+        System.out.println("czy ma posiadać symbole? t/n");
+        choice = scan.nextLine();
+        chars = choice.equals("t");
+        if(chars){
+            System.out.println("Podaj dozwolone znaki: ");
+            allowedChars = scan.nextLine();
+        }
+        System.out.println("Twoje hasło: " + new StrongPasswordGenerator(length,lower, upper, digits,chars, allowedChars).generate());
 
     }
 }
